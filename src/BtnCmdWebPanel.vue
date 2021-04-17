234c5834ff3206d283c20548eb2bf2ab0d2ca5ca
Minty Trebor <mintytrebor@gmail.com>
2021-04-17 12:56:52 +0100

    Alpha 0.8.14

---
+++ src/BtnCmdWebPanel.vue
@@ -0,0 +1,33 @@
+<style scoped>
+	.iframe-container {
+		position: relative;
+		overflow: auto;
+		background-color: transparent;
+	}
+	.iframe-container iframe {
+	position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		border: 0;
+	}
+</style>
+
+<template>
+	<div class="iframe-container">
+		<iframe :src="passedObject.altWebCamURL" ></iframe>
+	</div>		
+</template>
+
+<script>
+'use strict'
+
+export default {
+	props: {
+		passedObject: {
+			type: Object
+		}
+    },
+}
+</script>
