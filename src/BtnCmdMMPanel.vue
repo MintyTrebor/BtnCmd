43051cec1d838c7ec49d18881b3712bbe0726168
Minty Trebor <mintytrebor@gmail.com>
2021-10-20 15:35:10 +0100

    Beta 0.9.04

--- src/BtnCmdMMPanel.vue
+++ src/BtnCmdMMPanel.vue
@@ -1,35 +1,37 @@
 <template>
-	<v-card :key="'mmVal' + passedObject.panelMMPrefix + passedObject.panelID" :flat="passedObject.borderless" :color="passedObject.panelColor">
-		<v-card-text v-if="passedObject.panelMMOrientation == 'V'" class="text-center pb-1">
+	<v-card :key="'mmVal' + passedObject.panelMMPrefix + passedObject.panelID + passedObject.panelHSize + passedObject.panelWSize" :flat="passedObject.borderless" :height="passedObject.panelHSize" :width="passedObject.panelWSize" :color="passedObject.panelColor" style="height: 100%; width: 100%">
+		<v-row align="center" style="height: 100%; width: 100%" class="pa-0 ma-0">
+		<v-card-text v-if="passedObject.panelMMOrientation == 'V'" class="text-center pa-0 ma-0" align="center">
 			<v-row dense>
-				<v-col class="d-flex flex-column">
+				<v-col class="d-flex flex-column pa-0 ma-0">
 					<strong :style="'color: ' + passedObject.panelMMPrefixColor" :class="`text-${passedObject.panelMMTextSize}`">
 						{{ passedObject.panelMMPrefix }}
 					</strong>
 				</v-col>
 			</v-row>
 			<v-row v-if="passedObject.panelType == 'mmValue'" dense>
-				<v-col class="d-flex flex-column">
+				<v-col class="d-flex flex-column pa-0 ma-0">
 					<span :style="'color: ' + passedObject.panelMMValueColor" :class="`text-${passedObject.panelMMTextSize}`">
 						{{ matchedMMVal }}
 					</span>
 				</v-col>
 			</v-row>
 		</v-card-text>
-		<v-card-text v-else class="text-center pb-1">
+		<v-card-text v-else class="text-center pa-0 ma-0">
 			<v-row dense>
-				<v-col class="d-flex flex-column">
+				<v-col class="d-flex flex-column pa-0 ma-0">
 					<strong :style="'color: ' + passedObject.panelMMPrefixColor" :class="`text-${passedObject.panelMMTextSize}`">
 						{{ passedObject.panelMMPrefix }}
 					</strong>
 				</v-col>
-				<v-col v-if="passedObject.panelType == 'mmValue'" class="d-flex flex-column">
+				<v-col v-if="passedObject.panelType == 'mmValue'" class="d-flex flex-column pa-0 ma-0">
 					<span :style="'color: ' + passedObject.panelMMValueColor" :class="`text-${passedObject.panelMMTextSize}`">
 						{{ matchedMMVal }}
 					</span>
 				</v-col>
 			</v-row>
 		</v-card-text>
+		</v-row>
 	</v-card>		
 </template>
 
